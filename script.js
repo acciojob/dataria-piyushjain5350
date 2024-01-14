21324987a903e8e2c903afb7519b842dcbddb0f6
Piyush Jain <126599802+piyushjain5350@users.noreply.github.com>
2024-01-14 21:06:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-//your JS code here. If required.
+function mergeCraftDates(emilyCraftDates, oliverCraftDates) {
+  const mergedDates = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < emilyCraftDates.length && j < oliverCraftDates.length) {
+    if (emilyCraftDates[i] <= oliverCraftDates[j]) {
+      mergedDates.push(emilyCraftDates[i]);
+      i++;
+    } else {
+      mergedDates.push(oliverCraftDates[j]);
+      j++;
+    }
+  }
+
+  while (i < emilyCraftDates.length) {
+    mergedDates.push(emilyCraftDates[i]);
+    i++;
+  }
+
+  while (j < oliverCraftDates.length) {
+    mergedDates.push(oliverCraftDates[j]);
+    j++;
+  }
+
+  return mergedDates;
+}
+
+const emilyCraftDates = [3, 7, 12, 20];
+const oliverCraftDates = [1, 5, 8, 18, 25];
+const mergedDates = mergeCraftDates(emilyCraftDates, oliverCraftDates);
+
+console.log(mergedDates);
